6ebeea5c25bddd6636c9ad48ecbdfaacfe1e73f4
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-03-21 11:01:00 +0300

    working on difflib

--- dictionary.py
+++ dictionary.py
@@ -1,9 +1,18 @@
 import json
+import difflib
+
 
 def load_dictionary(data_json):
     with open(data_json, 'r') as file:
         data = json.load(file)
         return data
+
+def suggest_word(word, dictionary):
+    closest_match = difflib.get_close_matches(word, dictionary.keys(), n=1)
+    if closest_match:
+        return f"Did you mean '{closest_match[0]}' instead of '{word}'?"
+    else:
+        return "No close match found"
 
 def get_defination(word, dictionary):
     word = word.lower() #converts word to lowercase
@@ -12,4 +21,4 @@ def get_defination(word, dictionary):
     else: 
         return "Word not found"
     
-    
+
